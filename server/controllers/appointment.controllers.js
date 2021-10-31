c1c1f0cb8d37df82f598791591484f4141f525b2
MD <adlaon.mde@gmail.com>
2021-10-31 21:21:51 +0800

    Fixed bug not being able to set an appointment in some times due to for loop not working as expected

--- server/controllers/appointment.controllers.js
+++ server/controllers/appointment.controllers.js
@@ -13,79 +13,83 @@ const addAppointment = (req, res) => {
         $and: [{ $or: [{ firstName }, { email }, { phoneNumber }] }, { day }],
       },
     },
-  ]).then((result) => {
-    if (result.length !== 0) {
-      return res.status(403).json("User already has an appointment set");
-    }
+  ])
+    .then((result) => {
+      if (result.length !== 0) {
+        return res.status(403).json("User already has an appointment set");
+      }
 
-    //Check if the desired schedule is present in the database
-    AvailableSchedule.aggregate([
-      {
-        $match: {
-          year,
-          month,
-          day,
+      //Check if the desired schedule is present in the database
+      AvailableSchedule.aggregate([
+        {
+          $match: {
+            year,
+            month,
+            day,
+          },
         },
-      },
-    ])
-      .then((result) => {
-        if (result.length === 0) {
-          return res.status(406).json("No schedule found");
-        }
-
-        //Get the Object in Array result since there should only be 1 schedule
-        result = result[0];
-
-        for (let i = 0; i < result.schedule.length - 1; i++) {
-          if (result.schedule[i].time === time) {
-            let newSlotSize = Math.round(groupSize / result.persons);
-            if (newSlotSize === 0) newSlotSize = 1;
-
-            if (result.schedule[i].slots - newSlotSize < 0) {
-              return res.status(403).json("Insufficient slots for the group size");
-            }
-
-            if (result.schedule[i].slots === 0) {
-              return res.status(403).json("No slots available");
-            }
-
-            //Decrement the slots based on the user's group size
-            AvailableSchedule.updateOne(
-              { "schedule._id": result.schedule[i]._id },
-              { $inc: { "schedule.$.slots": -newSlotSize } }
-            )
-              .then((result) => {
-                //Add the appointment in the database
-                Appointment.create({ ...req.body, fullSchedule })
-                  .then((result) => {
-                    //Get the last 6 characters of the ID and set it as appointment_id to be easily reference later
-                    const appointment_id = result.id.slice(-6).toUpperCase();
-
-                    //Update the newly created document with its appointment_id
-                    Appointment.updateOne({ _id: result.id }, { $set: { appointment_id } })
-                      .then((result) => {
-                        return res.send({ ...result, appointment_id });
-                      })
-                      .catch((error) => {
-                        console.error(error);
-                      });
-                  })
-                  .catch((error) => {
-                    console.error(error);
-                  });
-              })
-              .catch((error) => {
-                console.error(error);
-              });
-
-            break;
+      ])
+        .then((result) => {
+          if (result.length === 0) {
+            return res.status(406).json("No schedule found");
           }
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  });
+
+          //Get the Object in Array result since there should only be 1 schedule
+          result = result[0];
+
+          for (let i = 0; i < result.schedule.length; i++) {
+            if (result.schedule[i].time === time) {
+              let newSlotSize = Math.round(groupSize / result.persons);
+              if (newSlotSize === 0) newSlotSize = 1;
+
+              if (result.schedule[i].slots - newSlotSize < 0) {
+                return res.status(403).json("Insufficient slots for the group size");
+              }
+
+              if (result.schedule[i].slots === 0) {
+                return res.status(403).json("No slots available");
+              }
+
+              //Decrement the slots based on the user's group size
+              AvailableSchedule.updateOne(
+                { "schedule._id": result.schedule[i]._id },
+                { $inc: { "schedule.$.slots": -newSlotSize } }
+              )
+                .then((result) => {
+                  //Add the appointment in the database
+                  Appointment.create({ ...req.body, fullSchedule })
+                    .then((result) => {
+                      //Get the last 6 characters of the ID and set it as appointment_id to be easily reference later
+                      const appointment_id = result.id.slice(-6).toUpperCase();
+
+                      //Update the newly created document with its appointment_id
+                      Appointment.updateOne({ _id: result.id }, { $set: { appointment_id } })
+                        .then((result) => {
+                          return res.send({ ...result, appointment_id });
+                        })
+                        .catch((error) => {
+                          console.error(error);
+                        });
+                    })
+                    .catch((error) => {
+                      console.error(error);
+                    });
+                })
+                .catch((error) => {
+                  console.error(error);
+                });
+
+              break;
+            }
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
 
 module.exports = { addAppointment };
